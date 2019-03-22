6ecd78e5e852c70f9d9dfb9295abc20ba9385ead
Gu Wenjia <1023478561@qq.com>
2019-03-22 19:04:04 +0800

    更新练习代码

--- c语言自学练习201903/c语言自学练习201903/lianxi.c
+++ c语言自学练习201903/c语言自学练习201903/lianxi.c
@@ -4,6 +4,26 @@
 #include <conio.h> //getch putch函数需要
 #include <string.h>
 
+//表2 运算符优先级和结合律
+//优先级	运算符	结合律
+//1	后缀运算符：[]    ()    ·    ->    ++    --(类型名称){列表}	从左到右
+//2	一元运算符：++    --    !    ~    +    -    *    &    sizeof_Alignof	从右到左
+//3	类型转换运算符：(类型名称)	从右到左
+//4	乘除法运算符：*    /    %	从左到右
+//5	加减法运算符：+    -	从左到右
+//6	移位运算符：<<    >>	从左到右
+//7	关系运算符：<<=    >>=	从左到右
+//8	相等运算符：==    !=	从左到右
+//9	位运算符 AND：&	从左到右
+//10	位运算符 XOR：^	从左到右
+//11	位运算符 OR：|	从左到右
+//12	逻辑运算符 AND：&&	从左到右
+//13	逻辑运算符 OR：||	从左到右
+//14	条件运算符：?:	从右到左
+//15	赋值运算符：
+//     =         +=        -=       *=       /=      %=       &=       ^=      |=   
+//   <<=      >>=	从右到左
+//16	逗号运算符：，	从左到右
 
 //c语言复试学习  自学练习
 ////赋值类型转换
@@ -141,6 +161,70 @@
 
 
 //输入某年某月，判断这一天是这一年的第几天？  应考虑闰年  月份大于3时，加一天
+
+//int getDaysByMonth(int m,int d,char flag)
+//{
+//	int c;
+//	if(flag=='r'){//闰年 
+//		if(m==2){//2月是29天
+//			return (31+d);
+//		}else if(m>2){
+//			c=0;
+//			m=m-1;
+//			for(;m>0;m--)
+//			{
+//				if(m==1||m==3||m==5||m==7||m==8||m==10||m==12){
+//					c+=31;
+//				}else if(m==2){
+//					c+=29;
+//				}else{
+//					c+=30;
+//				}
+//			}
+//			return c+d;
+//		}else{
+//			return d;
+//		}
+//	}else{//平年 
+//		if(m==2){//2月是28天
+//			return (31+d);
+//		}else if(m>2){
+//			c=0;
+//			m=m-1;
+//			for(;m>0;m--)
+//			{
+//				if(m==1||m==3||m==5||m==7||m==8||m==10||m==12){
+//					c+=31;
+//				}else if(m==2){
+//					c+=28;
+//				}else{
+//					c+=30;
+//				}
+//			}
+//			return c+d;
+//		}else{
+//			return d;
+//		}
+//	}	
+//}
+//void main()
+//{
+//	int y,m,d,count=1;
+//	printf("请输入年月日（例如2017-01-01）：");
+//	scanf("%d-%d-%d",&y,&m,&d);
+//	while(  ((((y%4==0 && y%100!=0)||(y%400==0)) && (m==2)) && d>29) || (y%400!=0 && y%4!=0 && m==2 && d>28) ||  ((m==1 || m==3 || m==5 || m==7 || m==8 || m==10 || m==12)&&d>31) || ((m==4 || m==6 || m==9 || m==11)&& d>30)){
+//		printf("请输入年月日（例如2017-01-01）：");
+//		scanf("%d-%d-%d",&y,&m,&d);
+//	}
+//	//先判断是否为闰年
+//	if((y%4==0 && y%100!=0)||(y%400==0)){//闰年 
+//		count=getDaysByMonth(m,d,'r');
+//	}else{//平年 
+//		count=getDaysByMonth(m,d,'n');
+//	} 
+//	printf("%d-%02d-%02d是今年的第%d天\n",y,m,d,count);
+//	putchar('\n');
+//}
 
 //第五章 循环控制结构程序
 //while语句  统计从键盘输入一行字符的个数
@@ -383,7 +467,7 @@
 //    strcat(dest, src);
 //    printf("dest:[%s]\n", dest);
 //	
-//	system("pause");
+//	 system("pause");
 //    return 0;
 //}
 
@@ -716,5 +800,216 @@
 //}
 //4
 
+//函数练习
+//void main()
+//{
+//int n;
+//printf("input number\n");
+//scanf("%d",&n);
+//s(n);
+//printf("n=%d\n",n);
+//system("pause"); 
+//}
+//int s(int n)
+//{
+//int i;
+//for(i=n-1;i>=1;i--)
+//n=n+i;
+//printf("n=%d\n",n);
+//}                 //n=15 n=5
 
+//
+//数组a中存放了一个学生5门课程的成绩，求平均成绩。
+//float aver(float a[5])
+//{
+//int i;
+//float av,s=a[0];
+//for(i=1;i<5;i++)
+//s=s+a[i];
+//av=s/5;
+//return av;
+//}
+//void main()
+//{
+//float sco[5],av;
+//int i;
+//printf("\ninput 5 scores:\n");
+//for(i=0;i<5;i++)
+//scanf("%f",&sco[i]);
+//av=aver(sco);						//数组名就是数组的首地址。因此在数组名作函数参数时所进行的传送只是地址的传送， 也就是说把实参数组的首地址赋予形参数组名。
+//printf("average score is %5.2f",av);
+//system("pause"); 
+//}
 
+//void nzp(int a[5])
+//{
+//int i;
+//printf("\nvalues of array a are:\n");
+//for(i=0;i<5;i++)
+//{
+//if(a[i]<0) a[i]=0;
+//printf("%d ",a[i]);
+//}
+//}
+//void main()
+//{
+//int b[5],i;
+//printf("\ninput 5 numbers:\n");
+//for(i=0;i<5;i++)
+//  scanf("%d",&b[i]);
+//printf("initial values of array b are:\n");
+//for(i=0;i<5;i++)
+// printf("%d ",b[i]);
+//nzp(b);
+//printf("\nlast values of array b are:\n");
+//for(i=0;i<5;i++)
+//printf("%d ",b[i]);
+//system("pause"); 
+//}
+
+//[例5.9]用递归法计算n!用递归法计算n!可用下述公式表示：
+//n!=1 (n=0,1)
+//n×(n-1)! (n>1)
+//按公式可编程如下：
+//long ff(int n)
+//{
+//long f;
+//if(n<0) printf("n<0,input error");
+//else if(n==0||n==1) f=1;
+//else f=ff(n-1)*n;
+//return(f);
+//}
+//void main()
+//{
+//int n;
+//long y;
+//printf("\ninput a inteager number:\n");
+//scanf("%d",&n);
+//y=ff(n);
+//printf("%d!=%ld",n,y);
+//system("pause"); 
+//}
+
+//指针
+//对于一个内存单元来说，单元的地址即为指针， 其中存放的数据才是该单元的内容。
+//在Ｃ语言中， 允许用一个变量来存放指针，这种变量称为指针变量。
+//因此， 一个指针变量的值就是某个内存单元的地址或称为某内存单元的指针。
+// 严格地说，一个指针是一个地址， 是一个常量。而一个指针变量却可以被赋予不同的指针值，是变。 
+//但在常把指针变量简称为指针。为了避免混淆，我们中约定：“指针”是指地址， 是常量，“指针变量”是指取值为地址的变量。 定义指针的目的是为了通过指针去访问内存单元。
+//int *p1;表示p1是一个指针变量，它的值是某个整型变量的地址。或者说p1指向一个整型变量。
+//(1)指针变量初始化的方法 
+//int a; int *p=&a;
+//(2)赋值语句的方法 int a;
+//int *p;
+//p=&a;
+//main(){
+//	int a=10,b=20,s,t,*pa,*pb;
+//	pa=&a;
+//	pb=&b;
+//	s=*pa+*pb;
+//	t=*pa**pb;
+//	printf("a=%d\nb=%d\na+b=%d\na*b=%d\n",a,b,a+b,a*b);   //a=10 b=20 a+b=30 a*b=200 s=30 t=200 
+//	printf("s=%d\nt=%d\n",s,t);
+//	system("pause"); 
+//}
+//main(){
+//int a,b,c,*pmax,*pmin;
+//printf("input three numbers:\n");
+//scanf("%d%d%d",&a,&b,&c);
+//if(a>b){
+//	pmax=&a;
+//	pmin=&b;}
+//else{
+//	pmax=&b;
+//	pmin=&a;}
+//if(c>*pmax) pmax=&c;
+//if(c<*pmin) pmin=&c;
+//printf("max=%d\nmin=%d\n",*pmax,*pmin);
+//system("pause"); 
+//}
+
+//
+//main(){
+//int a[5],i,*pa;
+//pa=a;
+//for(i=0;i<5;i++){
+//	*pa=i;
+//	pa++; //将指针pa指向a[]的下一个单元
+//}
+//
+//pa=a;
+//for(i=0;i<5;i++){
+//	printf("a[%d]=%d\n",i,*pa);
+//	pa++;
+//}
+//system("pause");
+//}
+//主函数
+//定义整型数组和指针
+//将指针pa指向数组a
+//循环
+//将变量i的值赋给由指针pa指向的a[]的数组单元
+//将指针pa指向a[]的下一个单元
+//......
+//指针pa重新取得数组a的首地址
+//循环
+//用数组方式输出数组a中的所有元素
+//将指针pa指向a[]的下一个单元
+//......
+
+//float aver(float *pa);//aver函数声明
+//main(){
+//float sco[5],av,*sp;
+//int i;
+//sp=sco;              //指针指向sco首地址
+//printf("\ninput 5 scores:\n");
+//for(i=0;i<5;i++) scanf("%f",&sco[i]);
+//av=aver(sp);                             //函数调用
+//printf("average score is %5.2f",av);
+//system("pause");
+//}
+//float aver(float *pa)
+//{
+//int i;
+//float av,s=0;
+//for(i=0;i<5;i++) s=s+*pa++;    
+//av=s/5;
+//return av;
+//}
+//下面通过例子来说明用指针形式实现对函数调用的方法。
+//int max(int a,int b){
+//if(a>b)return a;
+//else return b;
+//}
+//main(){
+//int max(int a,int b);   //1.从上述程序可以看出用，函数指针变量形式调用函数的步骤如下：1. 先定义函数指针变量，如后一程序中第9行 int (*pmax)();定义pmax为函数指针变量。
+//int(*pmax)();
+//int x,y,z;
+//pmax=max;              //2. 把被调函数的入口地址(函数名)赋予该函数指针变量，如程序中第11行 pmax=max;
+//printf("input two numbers:\n");
+//scanf("%d%d",&x,&y);
+//z=(*pmax)(x,y);         //3. 用函数指针变量形式调用函数，如程序第14行 z=(*pmax)(x,y);　调用函数的一般形式为： (*指针变量名) (实参表)
+//printf("maxmum=%d",z);
+//}
+
+////
+//int fun(int n){//判断n是否是素数 
+//    int i;
+//    for(i=2;i<n;i++)
+//        if(n%i==0) return 0;
+//    return 1;
+//}
+//int main()
+//{ 
+//    int n;
+//    scanf("%d",&n);
+//    if(fun(n))
+//        printf("%d是素数！\n",n);
+//    else 
+//        printf("%d不是素数！\n",n);
+//    
+//	system("pause");
+//	return 0; 
+//}
+
+ 
